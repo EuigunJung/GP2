cfca4e91eae01c653b38d25012d7b831dfac8cde
EuigunJung <63629484+EuigunJung@users.noreply.github.com>
2022-03-21 15:56:50 -0600

    Initial touches

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -8,9 +8,34 @@ namespace GP2.Controllers
 {
     public class HomeController : Controller
     {
+        private AppointmentContext appoinment { get; set; }
+
+        public HomeController(AppoinementContext_app)
+        {
+            appoinment = _app;
+        }
         public IActionResult Index()
         {
+            //This removes the key-value pairs, date & time in session:
+            HttpContext.Session.Remove("date");
+            HttpContext.Session.Remove("time");
             return View();
         }
+
+        //Sign Up 
+        public IActionResult SignUp(string currentdate)
+        {
+            //This removes the key-value pairs, date & time in session:
+            HttpContext.Session.Remove("date");
+            HttpContext.Session.Remove("time");
+
+            //Set the date to today if the currentdate is null:
+            if (currentdate == null)
+            {
+                currentdate = DateTime.Now.ToString("MM/dd/yyyy");
+            }
+            ViewBag.CurrentDate = currentdate;
+        }
+
     }
 }
